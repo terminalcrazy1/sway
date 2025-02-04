786becf2e997d5ac18bc8c6ead333d47dfa48916
terminalcrazy1 <liam.jbm2020@gmail.com>
2025-02-04 15:49:44 -0500

    .gitignore changes, store nixos central config via symlinks

---
+++ .config/nixos/program-configuration.nix
@@ -0,0 +1,28 @@
+# This configuration.nix contains system packages
+{ config, pkgs, lib, ... }:
+{
+# System Packages
+  boot.kernelPackages = pkgs.linuxPackages_testing;
+  fonts.packages = with pkgs; [
+    nerd-fonts.code-new-roman
+    nerd-fonts.roboto-mono
+  ];
+  environment.systemPackages = with pkgs; [
+    ranger
+    vim
+    ungoogled-chromium
+    bc
+    git
+    llama-cpp
+    htop
+  ];
+# Regular Packages
+  # Enable SwayWM
+  programs.sway = {
+    enable = true;
+  };
+  # Enable Foot
+  programs.foot = {
+    enable = true;
+  };
+}
